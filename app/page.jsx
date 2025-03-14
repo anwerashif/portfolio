b61e552d21e6110b12d43c653ae29e86d5dcb315
anwerashif <iamanwerashif@gmail.com>
2025-03-14 23:23:05 +0600

    added

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,8 @@
 import { FiDownload } from 'react-icons/fi';
-import { Button } from "@/components/ui/button";
 import Photo from "@/components/Photo";
 import Social from "@/components/Social";
 import Stats from '@/components/Stats';
+import Link from "next/link";
 
 const Home = () => {
   return <section className="h-full">
@@ -14,10 +14,10 @@ const Home = () => {
           <h1 className="h1 mb-6">Hello, I'm <br /><span className="text-accent">Anwer Ashif</span></h1>
           <p className="max-w-[500px] mb-9 text-white/80">I help businesses increase sales through high-converting landing pages, CRO (Conversion Rate Optimization), and seamless API integrations.</p>
           <div className="flex flex-col xl:flex-row items-center gap-8">
-            <Button variant="outline" size="lg" className="uppercase flex items-center gap-2 py-3">
+            <Link href="https://docs.google.com/document/d/1Jqte55JodJfN1rwC6_sVXopSzYog6HarDNb-SOUrDF4/edit?usp=sharing" className="justify-center whitespace-nowrap rounded-full font-semibold ring-offset-white transition-colors border border-accent bg-transparent text-accent hover:bg-accent hover:text-primary h[56px] px-6 text-sm tracking-[2px] uppercase flex items-center gap-2 py-3">
               <span>Download CV</span>
               <FiDownload className="text-xl" />
-            </Button>
+            </Link>
             <div className="mb-8 xl:mb-0">
               <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
             </div>
